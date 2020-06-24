7cb885a87593a8f33c69f9734ae95ca97f7b642d
mont-grunthal <65523578+mont-grunthal@users.noreply.github.com>
2020-06-24 16:25:07 -0400

    Added support for media. Added example backup output.

--- backup.py
+++ backup.py
@@ -13,18 +13,20 @@ external_urls = wc.external_urls
 dir_name = wc.dir_name
 url = wc.url
 
-
-#current date for recordkeeping
-today = date.today()
-date = today.strftime("%d/%m/%Y")
 #make parent and leaf dirs.
 os.makedirs(dir_name+"/info",exist_ok = True)
 os.makedirs(dir_name+"/html",exist_ok = True)
 os.makedirs(dir_name+"/media",exist_ok = True)
 
+
+#domain and date for recordkeeping in info.
+domain = urlparse(url).netloc
+today = date.today()
+date = today.strftime("%d/%m/%Y")
+
 #populate info folder with website, backup date, and links.
 f = open(dir_name + "/info/info.txt", "w+")
-f.write(f"Website Name: {url} \n")
+f.write(f"Website Name: {domain} \n")
 #TODO get time
 f.write(f"Backup date: {date} \n")
 f.write("Internal Links: \n")
@@ -38,26 +40,32 @@ f.close()
 
 
 #populate html folder with each pages html.
-for i,link in enumerate(internal_urls):
+for link in internal_urls:
     #download page
     parts = link.split('/');
     lastsegment = parts[-1];
     soup = BeautifulSoup(requests.get(link).content, "html.parser")
-    #creat .txt file for that page and names it
-    cur_dir = str(dir_name) + f"/html/{lastsegment}.html"
-    f = open(cur_dir, "w+")
-    f.write(f"Internal link number: {i} \n")
-    out = soup.prettify()
-    f.write(out)
 
+    #html#
 
+    #creates .txt file for that pages html
+    cur_dir_html = str(dir_name) + f"/html/{lastsegment}.html"
+    f_html = open(cur_dir_html, "w+")
+    #populate with pages html content.
+    html_out = soup.prettify()
+    f_html.write(html_out)
+    f_html.close()
 
-#for i,link in enumerate(internal_urls):
-#    #download page
-#    lastsegment = parts[-1];
-#    soup = BeautifulSoup(requests.get(link).content, "html.parser")
-#    cur_dir = str(dir_name) + f"/media/{lastsegment}.html"
-#    f = open(cur_dir, "w+")
-#    f.write(f"Internal link number: {i} \n")
-#    out = soup.prettify()
-#    f.write(out)
+    #media#
+
+    for img in soup.findAll(['img','image', 'iframe']):
+        os.makedirs(dir_name+f"/media/{lastsegment}",exist_ok = True)
+        #create file for that pages media
+        imgurl = url + img.get('src')
+        res = requests.get(imgurl)
+        parts = imgurl.split('/');
+        lastsegimg = parts[-1];
+        cur_dir_media = str(dir_name) + f"/media/{lastsegment}/{lastsegimg}"
+        f_media = open(cur_dir_media, "wb+")
+        for chunk in res.iter_content(100000):
+            f_media.write(chunk)
